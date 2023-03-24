6666a5c3b81ef30acabbfd56c7b9d29c3cb4db76
Zunair Pervaiz <zunair.pervaiz@techno-soft.com>
2023-03-24 13:50:57 +0500

    updated model

--- lib/models/users_model/users_model.dart
+++ lib/models/users_model/users_model.dart
@@ -55,23 +55,3 @@ class Datum {
         "avatar": avatar,
       };
 }
-
-class Support {
-  Support({
-    required this.url,
-    required this.text,
-  });
-
-  String url;
-  String text;
-
-  factory Support.fromJson(Map<String, dynamic> json) => Support(
-        url: json["url"],
-        text: json["text"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "url": url,
-        "text": text,
-      };
-}
